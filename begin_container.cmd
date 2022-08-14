30d25fdf5df75a4c81b4ad41fd8c73a72625d46e
Practice Golang <practice.golang@gmail.com>
2022-08-15 00:39:25 +0900

    Update begin_container.cmd

--- begin_container.cmd
+++ begin_container.cmd
@@ -1,7 +1,7 @@
+docker run --name oracle11g -d -p 1580:8080 -p 1521:1521 jaspeen/oracle-xe-11g
+
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SQLServer1433" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
 
 docker run --detach --name some-mariadb -p 13306:3306 --env MARIADB_USER=example-user --env MARIADB_PASSWORD= --env MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=true  mariadb:latest
-
-docker run --name oracle11g -d -p 1580:8080 -p 1521:1521 jaspeen/oracle-xe-11g
 
 docker run -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=pgsql -it --rm postgres
